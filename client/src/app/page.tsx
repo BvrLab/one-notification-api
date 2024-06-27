d78f36b27ed8a8e29873f29465a8de036d83855c
Finley <hongfeiz@mun.ca>
2024-06-27 14:18:35 -0300

    improve sideSideNavMenu

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
-// import NotificationForm from "../conponents/NotificationForm/NotificationForm";
+import NotificationForm from "../components/NotificationForm/NotificationForm";
+import SideNavMenu from "@/components/SideNavMenu/SideNavMenu";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>intor page</h1>
-      <h1>login</h1>
-      {/* <NotificationForm /> */}
+    // <main className="flex min-h-screen flex-col items-center justify-between">
+    <main>
+      {/* <h1>intor page</h1>
+      <h1>login</h1> */}
+
+      {/* <SideNavMenu /> */}
+      {/* <div className="w-full py-16 px-10">
+        <NotificationForm />
+      </div> */}
     </main>
   );
 }
--- client/src/components/SideNavMenu/SideNavMenu.tsx
+++ client/src/components/SideNavMenu/SideNavMenu.tsx
@@ -1,2 +1,140 @@
-//SideNavMenu
-import React from 'react';
+import React from "react";
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  TransitionChild,
+} from "@headlessui/react";
+
+import {
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/20/solid";
+
+import {
+  Bars3Icon,
+  BellIcon,
+  CalendarIcon,
+  ChartPieIcon,
+  Cog6ToothIcon,
+  DocumentDuplicateIcon,
+  FolderIcon,
+  HomeIcon,
+  UsersIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+
+import { NavigationOption } from "@/types";
+
+type SideNavMenuProp = {
+  navigations: NavigationOption[];
+  actions: NavigationOption[];
+  settings: NavigationOption[];
+};
+
+function classNames(...classes: (string | undefined | false | null)[]): string {
+  return classes.filter(Boolean).join(" ");
+}
+
+const SideNavMenu = ({ navigations, actions, settings }: SideNavMenuProp) => {
+  return (
+    <>
+      {/* Static sidebar for desktop */}
+      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+        {/* Sidebar component, swap this element with another sidebar if you like */}
+        <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-teal-950 px-6 pb-4">
+          <div className="flex h-16 shrink-0 items-center">
+            <img
+              className="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=white"
+              alt="Your Company"
+            />
+          </div>
+          <nav className="flex flex-1 flex-col">
+            <ul role="list" className="flex flex-1 flex-col gap-y-7">
+              <li>
+                <ul role="list" className="-mx-2 space-y-1">
+                  {navigations.map((item) => (
+                    <li key={item.name}>
+                      <a
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "bg-teal-900 text-white"
+                            : "text-indigo-200 hover:bg-teal-900 hover:text-white",
+                          "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6",
+                        )}
+                      >
+                        <item.icon
+                          className={classNames(
+                            item.current
+                              ? "text-white"
+                              : "text-indigo-200 group-hover:text-white",
+                            "h-6 w-6 shrink-0",
+                          )}
+                          aria-hidden="true"
+                        />
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+
+              <li className="mt-8">
+                <div className="text-xs font-semibold leading-6 text-indigo-200">
+                  Actions
+                </div>
+                <ul role="list" className="-mx-2 mt-2 space-y-1">
+                  {actions.map((action) => (
+                    <li key={action.name}>
+                      <a
+                        href={action.href}
+                        className={classNames(
+                          action.current
+                            ? "bg-teal-900 text-white"
+                            : "text-indigo-200 hover:bg-teal-900 hover:text-white",
+                          "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6",
+                        )}
+                      >
+                        <action.icon
+                          className={classNames(
+                            action.current
+                              ? "text-white"
+                              : "text-indigo-200 group-hover:text-white",
+                            "h-6 w-6 shrink-0",
+                          )}
+                          aria-hidden="true"
+                        />
+                        {action.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+
+              <li className="mt-auto">
+                <a
+                  href="#"
+                  className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-indigo-200 hover:bg-teal-900 hover:text-white"
+                >
+                  <Cog6ToothIcon
+                    className="h-6 w-6 shrink-0 text-indigo-200 group-hover:text-white"
+                    aria-hidden="true"
+                  />
+                  Settings
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SideNavMenu;
