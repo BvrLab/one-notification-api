0f0b00f9fc6f3a79dba8d8ea290f327f39c07183
Finley <hongfeiz@mun.ca>
2024-09-06 17:21:55 -0600

    add google oauth config

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,14 +5,21 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-gray-900">
+    <main className="flex min-h-screen flex-col items-center bg-gray-900">
     {/* <main> */}
+
+    <div className="mt-40">
         <Link 
             className="m-auto hover:bg-teal-800 rounded-md bg-teal-900 py-3 px-6 mx-2 text-base font-semibold text-white outline-none" 
             href="/dashboard/send-notification"
         >
             DEMO: Direct to dashbord/send-notification
         </Link>
+
+        <div className="mt-5 hover:bg-teal-800 rounded-md bg-teal-900 py-3 px-6 mx-2 text-base font-semibold text-white outline-none" >
+          Login
+        </div>
+    </div>
 
       {/* <h1>intor page</h1>
       <h1>login</h1> */}
