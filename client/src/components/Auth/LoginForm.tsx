1034cc72aa8d33c6fffdf1c0142652333a970963
Finley <hongfeiz@mun.ca>
2024-10-21 15:49:51 -0600

    add sign up form

--- client/src/components/Auth/LoginForm.tsx
+++ client/src/components/Auth/LoginForm.tsx
@@ -1,11 +1,15 @@
+import { useState } from 'react'
 import Button from '../Buttons/Button'
 import Container from '../Containers/Container'
 import Flexbox from '../Containers/flexbox/Flexbox'
 import { Devider } from '../Devider/Devider'
 import { LocalAuthForm } from './LocalAuthForm'
 import { SocialAuthForm } from './SocialAuthForm'
+import { LocalRegisterForm } from './LocalRegisterForm'
 
 export function LoginForm() {
+    const [isLogin, SetIsLogin] = useState(true)
+
     return (
         <Container>
             {/* Logo */}
@@ -14,7 +18,7 @@ export function LoginForm() {
                 Welcome to One Notification,
             </h2>
             <h3 className="mt-8 text-lg font-bold leading-6 tracking-tight text-gray-700">
-                Log in With
+                {isLogin ? 'Login' : 'Register'} With
             </h3>
 
             {/* Social Login */}
@@ -27,7 +31,7 @@ export function LoginForm() {
 
             {/* Password Login */}
             <Container className="mt-8">
-                <LocalAuthForm />
+                {isLogin ? <LocalAuthForm /> : <LocalRegisterForm />}
             </Container>
 
             <Flexbox justifyContent="between" flex="1" className="py-8">
@@ -35,15 +39,14 @@ export function LoginForm() {
                     <a
                         href="#"
                         className="text-black-500 font-semibold hover:text-amber-900"
+                        onClick={() => SetIsLogin(!isLogin)}
                     >
                         Don&apos;t have an account? Register
                     </a>
                 </Container>
 
                 <Container>
-                    <Button colorScheme="amber" className="rounded-3xl">
-                        Sign in
-                    </Button>
+                    <Button colorScheme="amber">Sign in</Button>
                 </Container>
             </Flexbox>
         </Container>
