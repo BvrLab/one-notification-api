ff3ffcf429cced346ea0b6ba611ec9f9edb39fc2
Finley <hongfeiz@mun.ca>
2024-10-30 15:05:10 -0600

    add vercel analytics

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 // import { Inter } from "next/font/google";
 import './globals.css'
+import { Analytics } from '@vercel/analytics/next'
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
     return (
         <html className="h-full bg-zinc-900" lang="en">
-            <body className="h-full">{children}</body>
+            <body className="h-full">{children} 
+                <Analytics/>
+            </body>
         </html>
     )
 }
