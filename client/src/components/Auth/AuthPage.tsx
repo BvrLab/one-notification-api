dfc4caf0a1593563caa92bd49f51f77f87c064d1
Finley <hongfeiz@mun.ca>
2024-11-01 15:24:20 -0600

    refine auth component

--- client/src/components/Auth/AuthPage.tsx
+++ client/src/components/Auth/AuthPage.tsx
@@ -1,6 +1,35 @@
-// src/app/auth/page.tsx
-import { AuthPageLayout } from '@/components/Auth/AuthPageLayout'
+import React from 'react'
+import Image from 'next/image'
+import Container from '../Containers/Container'
+import Flexbox from '../Containers/flexbox/Flexbox'
+import { LoginForm } from './LoginForm'
+
+
+
 
 export default function AuthPage() {
-    return <AuthPageLayout />
+    return (
+        <Flexbox className="min-h-full bg-amber-50" flex="1" direction="row">
+            <Flexbox
+                className="px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24"
+                flex="1"
+                direction="column"
+                justifyContent="center"
+            >
+                <Container className="mx-auto w-full max-w-sm lg:w-96">
+                    <LoginForm />
+                </Container>
+            </Flexbox>
+
+            <Container className="relative hidden w-0 flex-1 lg:block">
+                <Image
+                    alt=""
+                    src="/images/signin_page_picture.jpg"
+                    className="pointer-events-none absolute inset-0 h-full w-full select-none object-cover"
+                    width={1280}
+                    height={960}
+                />
+            </Container>
+        </Flexbox>
+    )
 }
--- client/src/components/Auth/LoginForm.tsx
+++ client/src/components/Auth/LoginForm.tsx
@@ -1,14 +1,263 @@
-'use client'
-
-import { useState } from 'react'
-import Button from '../Buttons/Button'
+import React from 'react'
 import Container from '../Containers/Container'
 import Flexbox from '../Containers/flexbox/Flexbox'
+import { useState } from 'react'
+import Button from '../Buttons/Button'
 import { Devider } from '../Devider/Devider'
-import { LocalAuthForm } from './LocalAuthForm'
-import { SocialAuthForm } from './SocialAuthForm'
-import { LocalRegisterForm } from './LocalRegisterForm'
 import { useRouter } from 'next/navigation'
+import { useFormState } from 'react-dom'
+import { signIn } from '@/lib/auth'
+import Input from '../Inputs/Input'
+import CheckboxGroup from '../Checkboxs/CheckboxGroup'
+import { signUp } from '@/lib/auth'
+import { GoogleButton } from './GoogleButton'
+import { GitHubButton } from './GitHubButton'
+
+interface LocalAuthFormProps {
+    setIsLogin: (isLogin: boolean) => void
+}
+
+interface LocalAuthFormProps {
+    setIsLogin: (isLogin: boolean) => void
+}
+
+function LocalRegisterForm({ setIsLogin }: LocalAuthFormProps) {
+    const [state, action] = useFormState(signUp, undefined)
+
+    return (
+        <Container>
+            <form action={action} method="POST" className="space-y-6">
+                {state?.message && (
+                    <p className="text-xs text-red-500">{state.message}</p>
+                )}
+
+                <Container>
+                    <Input
+                        label="Name"
+                        id="username"
+                        name="username"
+                        type="username"
+                        required
+                        autoComplete="name"
+                        colorScheme="amber"
+                    />
+                    {state?.error?.username && (
+                        <p className="text-sm text-red-500">
+                            {state.error.username}
+                        </p>
+                    )}
+                </Container>
+
+                <Container>
+                    <Input
+                        label="Email Address"
+                        id="email"
+                        name="email"
+                        // type="email"
+                        required
+                        autoComplete="email"
+                        colorScheme="amber"
+                    />
+
+                    {state?.error?.email && (
+                        <p className="text-xs text-red-500">
+                            {state.error.email}
+                        </p>
+                    )}
+                </Container>
+
+                <Container>
+                    <Input
+                        label="Password"
+                        id="password"
+                        name="password"
+                        // type="password"
+                        required
+                        autoComplete="current-password"
+                        colorScheme="amber"
+                    />
+                    {state?.error?.password && (
+                        <div className="text-xs text-red-500">
+                            <p>Password must:</p>
+                            <ul>
+                                {state.error.password.map((error) => (
+                                    <li key={error}>{error}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </Container>
+
+                <Flexbox
+                    alignItems="center"
+                    justifyContent="between"
+                    // className="flex items-center justify-between"
+                >
+                    <Container className="flex items-center">
+                        <CheckboxGroup
+                            onChange={() => {}}
+                            options={[
+                                {
+                                    id: 'remember-me',
+                                    isSelected: false,
+                                    name: 'Remember me',
+                                    value: 'Remember-me',
+                                },
+                            ]}
+                            colorScheme="amber"
+                        />
+                    </Container>
+
+                    <Container className="text-sm leading-6">
+                        <a
+                            href="#"
+                            className="font-semibold text-amber-600 hover:text-amber-500"
+                        >
+                            Forgot password?
+                        </a>
+                    </Container>
+                </Flexbox>
+
+                <Flexbox justifyContent="between" flex="1" className="py-8">
+                    <Container alignContent="center" className="text-sm">
+                        <a
+                            href="#"
+                            className="text-black-500 font-semibold hover:text-amber-900"
+                            onClick={(e) => {
+                                setIsLogin(true)
+                            }}
+                        >
+                            Already have an account? Login
+                        </a>
+                    </Container>
+
+                    <Container>
+                        <Button
+                            type="submit"
+                            className="mt-2 w-full"
+                            colorScheme="amber"
+                        >
+                            Register
+                        </Button>
+                    </Container>
+                </Flexbox>
+            </form>
+        </Container>
+    )
+}
+
+function LocalAuthForm({ setIsLogin }: LocalAuthFormProps) {
+    const [state, action] = useFormState(signIn, undefined)
+
+    return (
+        <Container>
+            <form action={action} method="POST" className="space-y-6">
+                <Container>
+                    {state?.message && (
+                        <p className="text-sm text-red-500">{state.message}</p>
+                    )}
+                    <Input
+                        label="Email Address"
+                        id="email"
+                        name="email"
+                        type="email"
+                        required
+                        autoComplete="email"
+                        colorScheme="amber"
+                    />
+                    {state?.error?.email && (
+                        <p className="text-sm text-red-500">
+                            {state.error.email}
+                        </p>
+                    )}
+                </Container>
+
+                <Container>
+                    <Input
+                        label="Password"
+                        id="password"
+                        name="password"
+                        type="password"
+                        required
+                        autoComplete="current-password"
+                        colorScheme="amber"
+                    />
+                    {state?.error?.password && (
+                        <p className="text-sm text-red-500">
+                            {state.error.password}
+                        </p>
+                    )}
+                </Container>
+
+                <Flexbox
+                    alignItems="center"
+                    justifyContent="between"
+                    // className="flex items-center justify-between"
+                >
+                    <Container className="flex items-center">
+                        <CheckboxGroup
+                            onChange={() => {}}
+                            options={[
+                                {
+                                    id: 'remember-me',
+                                    isSelected: false,
+                                    name: 'Remember me',
+                                    value: 'Remember-me',
+                                },
+                            ]}
+                            colorScheme="amber"
+                        />
+                    </Container>
+
+                    <Container className="text-sm leading-6">
+                        <a
+                            href="#"
+                            className="font-semibold text-amber-600 hover:text-amber-500"
+                        >
+                            Forgot password?
+                        </a>
+                    </Container>
+                </Flexbox>
+
+                <Flexbox justifyContent="between" flex="1" className="py-8">
+                    <Container alignContent="center" className="text-sm">
+                        <a
+                            href="#"
+                            className="text-black-500 font-semibold hover:text-amber-900"
+                            onClick={(e) => {
+                                e.preventDefault()
+                                setIsLogin(false)
+                            }}
+                        >
+                            Don&apos;t have an account? Register
+                        </a>
+                    </Container>
+
+                    <Container>
+                        <Button
+                            type="submit"
+                            className="mt-2 w-full"
+                            colorScheme="amber"
+                        >
+                            Login
+                        </Button>
+                    </Container>
+                </Flexbox>
+            </form>
+        </Container>
+    )
+}
+
+function SocialAuthForm() {
+    return (
+        <div>
+            <div className="mt-6 grid grid-cols-2 gap-12">
+                <GoogleButton>Google</GoogleButton>
+                <GitHubButton>GitHub</GitHubButton>
+            </div>
+        </div>
+    )
+}
 
 export function LoginForm() {
     const [isLogin, setIsLogin] = useState(true)
